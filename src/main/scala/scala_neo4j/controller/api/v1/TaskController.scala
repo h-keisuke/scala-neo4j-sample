4f74a1d84eaaec78db7f1ef8c8d92f40057dffc4
Keisuke HANAMURA <k-hanamura@bit-isle.co.jp>
2017-09-26 18:03:53 +0900

    implement support JSON request body

--- src/main/scala/scala_neo4j/controller/api/v1/TaskController.scala
+++ src/main/scala/scala_neo4j/controller/api/v1/TaskController.scala
@@ -1,14 +1,12 @@
 package scala_neo4j.controller.api.v1
 
-import org.json4s.jackson.Serialization.write
 import skinny.micro.WebApp
-import skinny.micro.contrib.json4s.JSONSupport
+import skinny.micro.contrib.json4s.{JSONParamsAutoBinderSupport, JSONSupport}
 import skinny.micro.response.{BadRequest, Created, NotFound, Ok}
 
-import scala_neo4j.resource.TaskResource
 import scala_neo4j.service.TaskService
 
-object TaskController extends WebApp with JSONSupport{
+object TaskController extends WebApp with JSONSupport with JSONParamsAutoBinderSupport{
 
   get("/api/v1/tasks/:id"){
     params.getAs[Long]("id") match {
@@ -28,8 +26,8 @@ object TaskController extends WebApp with JSONSupport{
   }
 
   post("/api/v1/tasks"){
-    params.get("title") match {
-      case Some(t) => params.get("description") match {
+    params.getAs[String]("title") match {
+      case Some(t) => params.getAs[String]("description") match {
         case Some(d) => Created(responseAsJSON(TaskService.create(t, d)),Map("Content-Type" -> "application/json"))
         case None => BadRequest
       }
